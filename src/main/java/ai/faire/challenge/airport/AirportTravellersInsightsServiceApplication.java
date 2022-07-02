798b179371bad66463789926f05431892acae9be
Gilberto Taccari <gilberto.taccari@gmail.com>
2022-07-02 16:19:54 +0200

    Setup Spring Boot project
    Configure Gradle build file and reformat the code.

---
+++ src/main/java/ai/faire/challenge/airport/AirportTravellersInsightsServiceApplication.java
@@ -0,0 +1,13 @@
+package ai.faire.challenge.airport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportTravellersInsightsServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AirportTravellersInsightsServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ai/faire/challenge/airport/AirportTravellersInsightsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.faire.challenge.airport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportTravellersInsightsServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
